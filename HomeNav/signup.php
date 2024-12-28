4087a2dc5b10de823bba53d6d402a6f18532f5f3
ShadiElattar <shadyalattar00@gmail.com>
2024-12-28 20:43:24 +0200

    didnt add much

--- HomeNav/signup.php
+++ HomeNav/signup.php
@@ -10,16 +10,20 @@
 <body>
     <?php 
     // did you submit or not
+
     //$_POST["submit"] -> true if you came in here through submission
     // if true 
     // validate
     //errors 
     // redirect to original page ?
+
+
+
+    // i will assume we come here with 100% validates info
     $server_name="localhost";
     $username = "root";
     $password = "";
     $dbname = "car_rental_system";
-
     $table_name="customer";
 
     $conn = new mysqli($server_name, $username, $password, $dbname);
@@ -28,7 +32,6 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully<br>";
     
     $sql = "SELECT * FROM ".$table_name; // selects all emails of all users from user table
     $result = $conn->query($sql); //to run a query through our connection
@@ -41,17 +44,11 @@
 
         while($row = $result->fetch_assoc()) { // fetch_assoc() funtion that gets you the each row in an array, each row is a dictionary
             if ($row["email"] == $_POST["email"]) { // comparing the each email form the table to the email we got from POST
-                // will have customer id increment and  email unique?
-                // same : we cant have that
-                //$errors[] = $row["email"]." is ALREADY TAKEN";
-                echo "<div>error</div>"
                 $taken=true;
                 break;
             }
         }
 
-    } else {
-        //$errors[] = "Table is empty: 0 results";// shouldnt be able to come here?
     }
     
 
@@ -60,11 +57,13 @@
         $sql = "INSERT INTO ".$table_name." (Name,email,phone_number,last_4_card_digits,password) VALUES ('".$_POST["name"]."','".$_POST["email"]."','".$_POST["phone"]."','".$_POST["card_num"]."','".md5($_POST["password"])."')";
         
         if ($conn->query($sql) === TRUE) { // can check if TRUE with certain queries including INSET, DELETE , UPDATE
-            echo "New record created successfully<br>";
-            echo '<a href="HomePage.html">Log in NOW!</a>';
+            header("Location: Homepage.html"); // will want to redirect to either a user page or admin page
+            // exit; // i think nn to exit?
         } else {
-            //$errors[] = "Error: creating new account failed: " . $sql . "<br>" . $conn->error;
+            echo "i think we have problem";
         }
+    } else {
+        echo "EMAIL IS DUPLICATED IDK WHERE ETO PUT THIS MESSAGE i think i will make seesiion to send it but need all php";
     }
 
     $conn->close();
